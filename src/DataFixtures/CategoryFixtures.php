0bf6ce36ba93f4a1da7682774ac39c3c3bb60860
Magali Perche <magali.per@hotmail.fr>
2022-11-30 11:40:04 +0100

    fixtures

---
+++ src/DataFixtures/CategoryFixtures.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Category;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class CategoryFixtures extends Fixture
+{
+    public const CATEGORIES =['Action', 'Aventure', 'Animation', 'Fantastique', 'Drame', 'Comédie', 'Horreur'];
+    public function load(ObjectManager $manager)
+    {
+        foreach(self::CATEGORIES as $key => $categoryName) {
+        $category = new Category();
+        $category->setName($categoryName);
+        $manager->persist($category);
+        $manager->flush();
+        }
+    }
+}
